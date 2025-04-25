09e6c97e8a6c088a988e6a1496e4a3a9354924d7
mojio <mohae@kakao.com>
2025-04-25 14:57:11 +0900

    연결 해제

--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ProfileCard } from './components/ProfileCard';
-export { default as CardData } from './components/CardData';
-export { default as IsOnline } from './components/IsOnline';
